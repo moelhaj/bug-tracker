b18ba983a2be2e83f1dc1ce3aba7e6160cf41c9f
Mohamed Elhaj <mohamed.elhajabujabr@roaa.tech>
2023-01-27 00:19:42 +0200

    hotfix/ hash router

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 
 import { Provider } from "react-redux";
@@ -13,9 +13,9 @@ root.render(
 	<Provider store={store}>
 		<PersistGate persistor={persister}>
 			<ContextProvider>
-				<BrowserRouter>
+				<HashRouter>
 					<App />
-				</BrowserRouter>
+				</HashRouter>
 			</ContextProvider>
 		</PersistGate>
 	</Provider>
