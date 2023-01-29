7297947bb4ca293b3c84e7c1245f643341928b10
Mohamed Elhaj <mohamed.elhajabujabr@roaa.tech>
2023-01-29 16:06:07 +0200

    hotfix / ux issues

--- frontend/src/components/dnd/Column.tsx
+++ frontend/src/components/dnd/Column.tsx
@@ -37,7 +37,7 @@ export default function Column({
 					dragOver === dragName
 						? "border border-indigo-600 ring-2 ring-indigo-600"
 						: "border-none ring-0",
-					"column-height y-scroll relative flex h-full flex-col gap-5 overflow-hidden overflow-y-scroll rounded-md bg-gray-100 pl-3 pt-3 pb-3 duration-300 dark:bg-gray-800"
+					"column-height y-scroll relative flex h-full flex-col gap-5 overflow-hidden overflow-y-scroll rounded-md bg-gray-100 pl-3 pt-3 pb-3 pr-3 duration-300 dark:bg-gray-800 lg:pr-0"
 				)}
 			>
 				<div className="flex flex-col gap-3">
