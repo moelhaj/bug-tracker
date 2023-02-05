a802159bdd31a1184cf0208171105422a4621636
Mohamed Elhaj <mohamed.elhajabujabr@roaa.tech>
2023-02-05 16:08:48 +0200

    hotfix/darkmode

--- frontend/src/pages/dashboard/Block.tsx
+++ frontend/src/pages/dashboard/Block.tsx
@@ -16,7 +16,7 @@ export default function Block({ data, icon, title, type }: Props) {
 				<h1 className="text-lg font-bold">{title}</h1>
 			</div>
 			{data?.length < 1 && (
-				<div className="mt-3 flex flex-col items-center gap-3 rounded-md border border-gray-200 bg-white py-10 text-sm text-gray-400 dark:border-none dark:bg-gray-800">
+				<div className="mt-3 flex flex-col items-center gap-3 rounded-md border border-gray-200 bg-white py-10 text-sm text-gray-400 dark:border-none dark:bg-gray-700">
 					<p className="">{`No ${type} found`}</p>
 				</div>
 			)}
@@ -24,7 +24,7 @@ export default function Block({ data, icon, title, type }: Props) {
 				{data?.map((item: any) => (
 					<div
 						key={item.id}
-						className="rounded-md border border-gray-200 bg-white p-3 dark:border-none dark:bg-gray-800"
+						className="rounded-md border border-gray-200 bg-white p-3 dark:border-none dark:bg-gray-900"
 					>
 						<p className="text-lg font-bold">{item.title}</p>
 						<p className="text-sm text-gray-600 dark:text-gray-300">{item.status}</p>
