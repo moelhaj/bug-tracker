78fa88e66b2ce04cf853da2b630d4ea15ffd0018
Mohamed Elhaj <mohamed.elhajabujabr@roaa.tech>
2023-02-28 16:13:29 +0200

    hotfix / background metrics

--- backend2/src/api/assigned/assigned.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-import * as services from "./assigned.services";
-
-export const getAll = async (req: Request, res: Response) => {
-	const workItems = await services.find(req.id);
-	return res.status(200).send(workItems);
-};
--- backend2/src/utils/catchAsync.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable indent */
-import { Request, Response, NextFunction } from "express";
-
-const catchAsync =
-	(fn: any): any =>
-	(req: Request, res: Response, next: NextFunction) =>
-		Promise.resolve(fn(req, res, next)).catch(next);
-
-export default catchAsync;
